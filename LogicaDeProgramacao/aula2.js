466bf0f062177354916e06b8d20e187b37e55055
Lp <pinheiroleandro7009@gmail.com>
2026-08-12 20:38:35 +0100

    Delete LogicaDeProgramacao directory

--- LogicaDeProgramacao/aula2.js
+++
@@ -1,6 +0,0 @@
-//concatenacao de variaveis
-var idade = 16;
-var nome = 'Leandro';
-var nome1 = 'Deus';
-var dirige = !true;
-console.log(`o nome formado é ${nome} e ${nome1} idade ${idade} dirige: ${dirige}`);

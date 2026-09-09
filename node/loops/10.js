c155321bcb87e9f6a28c7dec220f0b3a16aa0cf2
Leandro Jesus <pinheiroleandro7009@gmail.com>
2026-09-10 00:16:02 +0100

    feat: exercico for loops

---
+++ node/loops/10.js
@@ -0,0 +1,13 @@
+let isMorning = true;
+const name = "Leandro";
+
+for (let i = 1; i <= 3; i++) {
+    console.log(i)
+
+}
+
+if (!isMorning) {
+    console.log(`Good Morning, ${name}`);
+}else {
+    console.log(`Good Night, ${name}`);
+}
---
+++ node/loops/9.js
@@ -0,0 +1,9 @@
+let contador = 3;
+const nome = "Leandro";
+
+do {
+    console.log(contador)
+    contador--;
+} while(contador > 0)
+
+    console.log("welcome to new year")

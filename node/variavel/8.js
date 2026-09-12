626cc98115aebdf46dc98ec4721dc74d4785ee51
Leandro Jesus <pinheiroleandro7009@gmail.com>
2026-09-13 00:19:50 +0100

    feat: exercicio  node/variavel/8.js

---
+++ node/variavel/8.js
@@ -0,0 +1,30 @@
+let pessoas = [
+{
+	"id": 0,
+	"nome": "Leandro",
+	"apelido": "Jesus",
+	"idade": 16,
+	"pais": "Portugal",
+	"morada": "Rua Santo Antao",
+	"escola": "Escola Segundaria Fernao Mendes Pinto"
+},
+{
+	"id": 1,
+	"nome": "Edvaldo",
+	"apelido": "Jesus",
+	"idade": 10,
+	"pais": "Portugal",
+	"morada": "Rua Agosto De Afonso",
+	"escola": "Escola Segundaria Professor Rui Luis Gomes"
+},
+{
+	"id": 2,
+	"nome": "Esmy",
+	"apelido": "Jesus",
+	"idade": 18,
+	"pais": "Portugal",
+	"morada": "Rua Santo Chiki Chiki"
+}
+];
+
+console.log(pessoas);

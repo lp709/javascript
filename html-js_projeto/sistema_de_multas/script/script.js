f8ebd138eec7269b7d444d0955f83305e8e3301a
Leandro Jesus <pinheiroleandro7009@gmail.com>
2026-09-14 20:18:05 +0100

    feat: centarlizei horizontal e verticalmente, adicionei borda na tag main e corrigi syntax do javascript

--- html-js_projeto/sistema_de_multas/script/script.js
+++ html-js_projeto/sistema_de_multas/script/script.js
@@ -1,18 +1,24 @@
 let input = document.querySelector('#vel');
 let message = document.querySelector('.message');
+let ver = document.querySelector('#myBtn');
 
 input.addEventListener('input', () => {
-
-    //message.textContent = input.value; 
-
-    if (input.value >= 80) {
-        message.textContent = "Voce Foi Multado Por Execo De Velocidade";
-    } else if (input.value < 79) {
-        message.textContent = "Voce Esta No Limite Tenha Uma Boa Viagem";
-    }
 
     if (input.value == "") {
         message.textContent = "";
     }
 
 });
+
+ver.addEventListener('click', () => {
+
+    if (input.value == "") {
+        message.textContent = "";
+    } else if (input.value >= 80) {
+        message.textContent = "Voce Foi Multado Por Execo De Velocidade";
+    } else {
+        message.textContent = "Voce Esta No Limite Tenha Uma Boa Viagem";
+    }
+
+});
+

5276d9f1df550482b2790bbcdeb8aeea3c5c003d
lp <pinheiroleandro7009@gmail.com>
2026-09-16 00:00:15 +0100

    feat: exercicio

---
+++ node/variavel/9.js
@@ -0,0 +1,11 @@
+let nome, idade, escola, all;
+
+nome = "Leandro";
+idade = 16;
+idade = Number(idade);
+escola = "Esola Segundaria Fernao Mendes Pinto";
+
+console.log(`Nome: ${nome}`);
+console.log(`Idade: ${idade}`);
+console.log(`Escola: ${escola}`);
+

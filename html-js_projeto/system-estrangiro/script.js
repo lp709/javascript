ad80f20dda117f491945416e7aa8122860926e45
Leandro Jesus <pinheiroleandro7009@gmail.com>
2026-09-19 23:58:26 +0100

    feat: adicionei o array com 6 objects dentro, cada objeto tem o id, pais e nacionalidade, eu vou meter isso tudo como informacaode cada cidadao

--- html-js_projeto/system-estrangiro/script.js
+++ html-js_projeto/system-estrangiro/script.js
@@ -1,8 +1,42 @@
-let country = "Portugal";
-country = "Espanha"
+let country = [
+    {
+        "id": "pt-PT",
+        "pais": "Portugal",
+        "nacionalidade": "Portuges"
+    },
+    {
+        "id": "pt-BR",
+        "pais": "Brazil",
+        "nacionalidade": "Brazileiro"
+    },
+    {
+        "id": "es-ES",
+        "pais": "Espanha",
+        "naciolidade": "Espanhol"
+    },
+    {
+        "id": "fr-FR",
+        "pais": "Franca",
+        "nacionalidade": "Frances"
+    },
+    {
+        "id": "en-US",
+        "pais": "Estados Unidos De America",
+        "nacionalidade": "Americano"
+    },
+    {
+        "id": "en-UK",
+        "pais": "Reino Unido",
+        "nacionalidade": "Ingles"
+    }
+]
 
-if (country === "Portugal") {
-    console.log(`Voce e de ${country}`);
-} else {
-    console.log("Voce e estrangeiro")
+if(!country[0]){
+    console.log(`Codigo De Pais: ${country[0].id}`);
+    console.log(`Pais: ${country[0].pais}`);
+    console.log(`Nacionalidade: ${country[0].nacionalidade}`);
+} else if(country[1]){
+    console.log(`Codigo De Pais: ${country[1].id}`);
+    console.log(`Pais: ${country[1].pais}`);
+    console.log(`Nacionalidade: ${country[1].nacionalidade}`);
 }
---
+++ node/operadores/1/script.js
@@ -0,0 +1,15 @@
+let numeros, calc;
+
+numeros = [
+    104,
+    240,
+    346,
+    543,
+    100,
+    150
+];
+
+calc = numeros[2] % numeros[4];
+
+console.log(numeros.length);
+console.log(calc);
